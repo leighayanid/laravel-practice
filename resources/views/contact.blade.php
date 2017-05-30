0928fd342583e1f3bc1be8ca9dae3f984adaede3
Leigh Dinaya <leighdinaya04@gmail.com>
2017-05-30 17:13:25 +0800

    play with partials

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,11 @@
-<div class="container">
-	<h4>Contact Page {{ $email }}</h4>
-	@foreach($contactNumber as $number)
-		<ul class="list-group">
-			<li class="list-group-item">{{ $number }}</li>
-		</ul>
-	@endforeach
-</div>
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+		<h4>Contact Page {{ $email }}</h4>
+			<ul class="list-group">
+				@foreach($contactNumber as $number)
+						<li class="list-group-item">{{ $number }}</li>
+				@endforeach
+			</ul>
+	</div>
+@endsection
